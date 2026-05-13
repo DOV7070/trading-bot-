3fcd5f806b3a3fbf75faf926b26ea2a14c111c8a
DOV7070 <dovesebban@icloud.com>
2026-05-13 05:26:17 +0200

    Update trader.py

--- core/trader.py
+++ core/trader.py
@@ -13,8 +13,9 @@ from binance.exceptions import BinanceAPIException
 from core.config import config
 from core.binance_client import get_client, get_klines, get_price
 from core.risk import Position, position_size
-try:    from core.strategies import get_strategy except    ModuleNotFoundError: 
-
+try:
+    from core.strategies import get_strategy
+except    ModuleNotFoundError:    
 from strategies import get_strategy
 log = logging.getLogger(__name__)
 
